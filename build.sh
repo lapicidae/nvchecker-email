5d15e0d69512c4abc0ecf262d7b513ba06f4378b
lapicidae <andreas.hemmerle@gmail.com>
2025-07-05 07:13:31 +0200

    no interactive TTY is required

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 imageTag=${imageTag:-"nvchecker-email"}
 
-printf -v nvcVersion '%s' "$(docker run -it --rm alpine:latest sh -c "apk update —no-progress --quiet && apk info nvchecker | head -n 1 | cut -d - -f 2 | tr -d '\n'")"
+printf -v nvcVersion '%s' "$(docker run --rm alpine:latest sh -c "apk update —no-progress --quiet && apk info nvchecker | head -n 1 | cut -d - -f 2 | tr -d '\n'")"
 printf -v dateTime '%(%Y-%m-%dT%H:%M:%S%z)T'
 printf -v nvcRevision '%s' "$(git ls-remote -t 'https://github.com/lilydjwg/nvchecker.git' "v${nvcVersion}" | cut -f 1)"
 printf -v baseDigest '%s' "$(docker image pull alpine:latest | grep -i digest | cut -d ' ' -f 2)"
